0a1b5f3abf206a53c8a1929818cda099ee597fb0
Yvan Salcedo <69168426+drakgons@users.noreply.github.com>
2023-06-26 18:38:31 -0500

    Update page.js

--- src/app/[id]/page.js
+++ src/app/[id]/page.js
@@ -1,5 +1,5 @@
 const Home = async ({ params }) => {
-  const pokemon = await fetch(`http://localhost:3000/api/pokemon/${params.id}`)
+  const pokemon = await fetch(`https://pokemon-jsonfile-api.vercel.app/api/pokemon/${params.id}`)
     .then((res) => res.json())
     .catch((err) => console.log(err));
 
